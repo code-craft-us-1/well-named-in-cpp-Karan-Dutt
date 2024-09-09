ac59da7b8ec45b71aa1fd3e15563437731ac8640
Karan Dutt <karan.dutt@philips.com>
2024-09-09 10:23:55 +0530

    updated namespace

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -32,7 +32,7 @@ namespace TelCoColorCoder
         return minorColor;
     }
 
-    ColorPair TelCoColorCoder::GetColorFromPairNumber(int pairNumber) {
+    ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor =
             (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
@@ -40,7 +40,7 @@ namespace TelCoColorCoder
             (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
         return ColorPair(majorColor, minorColor);
     }
-    int TelCoColorCoder::GetPairNumberFromColor(MajorColor major, MinorColor minor) {
+    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }
 
